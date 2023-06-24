b276acd6ed3bd3b7ca371968923b52d9f26cc9eb
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-06-24 18:51:30 +0530

    fix gdp calculation

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,78 @@
 import 'dotenv/config';
 import { request } from 'undici';
 import cron from 'node-cron';
-import Big from 'big.js';
 import client from './client';
 import logger from './utils/logger';
 
 const getGdp = async (): Promise<string> => {
-  const {
-    body,
-  } = await request('https://opensheet.elk.sh/1I6EEV3RTTPTI5ugX3IWvkjx39pjSym9tk4DBeoXyGys/Bounties%20Paid');
+ const {
+  body,
+ } = await request('https://socftnkojidkvtmjmyha.supabase.co/storage/v1/object/public/earnings/projects.json');
+ if (!body) {
+  logger.error('Error fetching data');
+ }
 
-  const superteam_earnings = await body.json();
+ const superteam_projects = await body.json();
 
-  const earnings_arr = superteam_earnings
-    .map((x: Record<string, string>) => x['Total Earnings USD']
-      ?.replace(',', ''))
-    .filter((x: string) => !!x)
-    .map((x: string) => {
-      try {
-        if (x.includes('$')) {
-          x = x.replace('$', '');
-        }
-        return Big(x);
-      } catch (err) {
-        logger.info(x);
-        logger.error(err);
-        return Big(0);
-      }
-    });
+ const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+ });
 
-  const gdp = earnings_arr.reduce((acc: Big, curr: Big) => curr.add(acc), 0);
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-
-    // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    // minimumFractionDigits: 0,
-    maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-  });
-
-  return formatter.format(gdp);
+ return formatter.format(Math.round(
+  superteam_projects.reduce(
+   (sum: number, project: any) => sum + (project.fields['Total Earnings USD'] || 0),
+   0,
+  ),
+ ));
 };
 
 const setGdp = async () => {
-  const gdp = await getGdp();
+ const gdp = await getGdp();
 
-  const superteam_guild_id = process.env.GUILD_ID as string;
-  const guild = await client.guilds.cache.get(superteam_guild_id);
+ const superteam_guild_id = process.env.GUILD_ID as string;
+ const guild = await client.guilds.cache.get(superteam_guild_id);
 
-  if (!guild) {
-    logger.error('No guild found');
-    return;
-  }
+ if (!guild) {
+  logger.error('No guild found');
+  return;
+ }
 
-  logger.info(gdp);
+ logger.info(gdp);
 
-  //  guild.me?.setNickname(gdp);
-  //  logger.info(`Done setting nickname to ${gdp}`);
+ //  guild.me?.setNickname(gdp);
+ //  logger.info(`Done setting nickname to ${gdp}`);
 
-  const channel = guild.channels.cache.find(chnl => chnl.name.includes('GDP:'));
+ const channel = guild.channels.cache.find(chnl => chnl.name.includes('GDP:'));
 
-  if (channel) {
-    channel.setName(`📈 | GDP: ${gdp}`);
-    logger.info(`📈 | GDP: ${gdp}`);
-    logger.info('Updated channel name');
-  } else {
-    logger.info('Channel Doesn\'t exist');
-  }
+ if (channel) {
+  channel.setName(`📈 | GDP: ${gdp}`);
+  logger.info(`📈 | GDP: ${gdp}`);
+  logger.info('Updated channel name');
+ } else {
+  logger.info('Channel Doesn\'t exist');
+ }
 };
 
 cron.schedule('*/30 * * * *', async () => {
-  const token = process.env.DISCORD_TOKEN;
+ const token = process.env.DISCORD_TOKEN;
 
-  if (!token) {
-    logger.error('No token found');
-    return;
+ if (!token) {
+  logger.error('No token found');
+  return;
+ }
+
+ await client.login(token);
+
+ client.on('ready', async () => {
+  logger.info(`Logged in as ${client?.user?.tag}!`);
+  try {
+   await setGdp();
+  } catch (error) {
+   logger.error(error);
   }
-
-  await client.login(token);
-
-  client.on('ready', async () => {
-    logger.info(`Logged in as ${client?.user?.tag}!`);
-    try {
-      await setGdp();
-    } catch (err) {
-      console.log(err)
-    }
-  });
+ });
 });
